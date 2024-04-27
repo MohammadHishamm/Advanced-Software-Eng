9bceb3470538600ba753c6b7ba1d05e584aac580
MohammadHishamm <mohammad2109652@miuegypt.edu.eg>
2024-04-27 16:00:05 +0300

    teacher form validation update

--- demo/src/main/java/com/example/demo/models/Instructor.java
+++ demo/src/main/java/com/example/demo/models/Instructor.java
@@ -37,7 +37,7 @@ public class Instructor
     
     private String Country;
 
-    @NotEmpty(message = "Position is required")
+    @NotEmpty(message = "CV is required")
     private String Pdf; 
 
     private String Comment;
