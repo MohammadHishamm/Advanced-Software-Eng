2a489ecd96a0decf5e6e9f25c46acc73bc4eaabc
zeyad <zeiadabdelnasserelzayaty@gmail.com>
2024-05-01 16:09:04 +0300

    view_tutor page setup

--- demo/src/main/java/com/example/demo/controller/adminController.java
+++ demo/src/main/java/com/example/demo/controller/adminController.java
@@ -1,16 +1,48 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import com.example.demo.models.Instructor;
+import com.example.demo.repositories.InstructorRepository;
+import com.example.demo.repositories.UserRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 @RequestMapping("/admin")
 @RestController
 public class adminController {
+
+    @Autowired  
+    private InstructorRepository instructorRepository;
+    
+    @Autowired  
+    private UserRepository userRepository;
+    
     @GetMapping("")
     public ModelAndView getpage() {
         ModelAndView mav = new ModelAndView("Admin/index.html");
+        return mav;
+    }
+
+    @GetMapping("/view-tutor")
+    public ModelAndView view_tutor( HttpSession session) 
+    {
+        ModelAndView mav = new ModelAndView("Admin/tutor.html");
+        List<Instructor> instructorList = this.instructorRepository.findAll(); 
+        if(session != null && session.getAttribute("type").toString() == "admin")
+        {
+            mav.addObject("instructorlist", instructorList);
+        }
+        else
+        {
+            mav = new ModelAndView("Signup-in.html");
+        }
+        
         return mav;
     }
 }
