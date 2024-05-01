06232c5fc19c97773a5bbd1fd49447d9dc121e91
MohammadHishamm <mohammad2109652@miuegypt.edu.eg>
2024-05-01 18:03:26 +0300

    user controller updated

--- demo/src/main/java/com/example/demo/repositories/CartRepository.java
+++ demo/src/main/java/com/example/demo/repositories/CartRepository.java
@@ -3,7 +3,9 @@ package com.example.demo.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.models.Cart;
+import com.example.demo.models.Student;
+
 
 public interface CartRepository extends JpaRepository<Cart, Integer> {
-    
+    Cart findByStudent(Student student);
 }
